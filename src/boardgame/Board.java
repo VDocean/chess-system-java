382c9a7ed6db26a71836797609392694e3c76686
vitoria nascimento <vivanascimento2020@gmail.com>
2024-04-08 09:46:20 -0300

    placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,14 +6,44 @@ public class Board {
 	private Piece[][] pieces;
 	
 	public Board(int rows, int columns) {
-		super();
+		
 		this.rows = rows;
 		this.columns = columns;
 		pieces=new Piece[rows][columns];
 		
 	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
  	
+	//nao fiz um metodo get e set para pieces, pois elas devem ser retornadas de
+	//forma individual e não a matriz toda de uma vez
 	
+	public Piece piece(int row,int column) {
+		return pieces[row][column];
+	}
+	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
+	
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()]=piece; // atribui a minha matriz de peças a peca passada como argumento
+		piece.position=position;
+	}
 	
 
 }
--- src/chess_system/Program.java
+++ src/chess_system/Program.java
@@ -1,15 +1,16 @@
 package chess_system;
 
-import boardgame.Board;
-import boardgame.Position;
+
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
 		
-		Board board=new Board(8,8);
-        
+			
+		ChessMatch chessMatch=new ChessMatch();
+		
+        UI.printBoard(chessMatch.getPieces());
 		
 	}
 
