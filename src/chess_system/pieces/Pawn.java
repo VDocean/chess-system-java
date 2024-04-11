fc4069ef121d4262653a9c1a67520b5157fcc6dc
Nascimento <vitoria.a.nascimento@capgemini.com>
2024-04-11 12:54:55 -0300

    Queen

--- src/chess_system/pieces/Pawn.java
+++ src/chess_system/pieces/Pawn.java
@@ -11,6 +11,7 @@ public class Pawn extends ChessPiece{
 		super(board, color);
 	}
     @Override
+    
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 
@@ -41,7 +42,7 @@ public class Pawn extends ChessPiece{
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 			p.setValues(position.getRow() + 2, position.getColumn());
-			Position p2 = new Position(position.getRow() - 1, position.getColumn());
+			Position p2 = new Position(position.getRow() + 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
