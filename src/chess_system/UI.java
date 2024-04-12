4fc5c70fd323d8933fb115f44eda87285e71e4a6
Nascimento <vitoria.a.nascimento@capgemini.com>
2024-04-12 11:09:34 -0300

    Special move- En Passant

--- src/chess_system/UI.java
+++ src/chess_system/UI.java
@@ -48,6 +48,7 @@ public class UI {
 		char column=s.charAt(0);
 		int row=Integer.parseInt(s.substring(1));
 		return new ChessPosition(column,row);
+		
 		}catch(RuntimeException e) {
 			throw new InputMismatchException("Error reading chess position.");
 		}
